e591224fa5beee8866f99cba7447e135f2e6c0e1
Jabir <jabirhussainy@gmail.com>
2023-10-18 00:39:28 +0530

    login/signup page created, set routes to it

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  const banner = "/images/home1.jpg"
   let places = [
     {
       location: "Alappuzha",
@@ -30,7 +30,16 @@ router.get('/', function (req, res, next) {
       image: "/images/home1.jpg"
     }
   ]
-  res.render('index', { places });
+  res.render('index', { places, banner });
 });
+router.get('/login',(req,res)=>{
+  res.render('login')
+})
+router.get('/signup',(req,res)=>{
+  res.render('signup')
+})
+router.get('/back',(req,res)=>{
+  res.redirect('/')
+})
 
 module.exports = router;
