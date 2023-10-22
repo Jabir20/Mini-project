b503da59edcf54a13cba19751b4ba59d7b6aeb0e
Jabir <jabirhussainy@gmail.com>
2023-10-22 23:37:01 +0530

    modified the banner to automatically change every 2sec

---
+++ public/javascript/script.js
@@ -0,0 +1,17 @@
+const slider = document.querySelector('.slider');
+const sliderWidth = slider.clientWidth;
+let currentSlide = 1;
+
+function scrollToNextSlide() {
+    currentSlide++;
+    if (currentSlide > 4) {
+        currentSlide = 1;
+    }
+    const scrollAmount = (currentSlide - 1) * sliderWidth;
+    slider.scroll({
+        left: scrollAmount,
+        behavior: 'smooth'
+    });
+}
+
+setInterval(scrollToNextSlide, 2000); // Scroll every 2 seconds
--- routes/user.js
+++ routes/user.js
@@ -6,31 +6,34 @@ const { response } = require('../app');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  const banner = "/images/banner.jpg"
+  const banner1 = "/images/banner-image1.jpg"
+  const banner2 = "/images/banner-image2.jpg"
+  const banner3 = "/images/banner-image3.jpg"
+  const banner4 = "/images/banner-image4.jpg"
   productHelper.getAllProducts().then((places) => {
 
-    res.render('user/view-location', { places, banner, user: true, footer:true, Account: true});
+    res.render('user/view-location', { places, banner1, banner2, banner3, banner4, user: true, footer: true, Account: true });
   })
 });
 router.get('/login', (req, res) => {
-  res.render('user/login', { user: true})
+  res.render('user/login', { user: true })
 })
 router.get('/signup', (req, res) => {
-  res.render('user/signup', { user: true})
+  res.render('user/signup', { user: true })
 })
 router.get('/back', (req, res) => {
   res.redirect('/')
 })
-router.post('/signup',(req,res)=>{
-  userHelper.doSignup(req.body).then((response)=>{
+router.post('/signup', (req, res) => {
+  userHelper.doSignup(req.body).then((response) => {
     console.log(response);
   })
 })
-router.post('/login',(req,res)=>{
-  userHelper.doLogin(req.body).then((response)=>{
-    if(response.status){
+router.post('/login', (req, res) => {
+  userHelper.doLogin(req.body).then((response) => {
+    if (response.status) {
       res.redirect('/')
-    }else{
+    } else {
       res.redirect('/login')
     }
   })
