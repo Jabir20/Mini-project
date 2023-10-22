98cebd1a5cad89b338e3dab5da309117a9d42d3b
Jabir <jabirhussainy@gmail.com>
2023-10-22 12:48:00 +0530

    Made minor changes

--- routes/user.js
+++ routes/user.js
@@ -7,17 +7,16 @@ router.get('/', function (req, res, next) {
   const banner = "/images/banner.jpg"
   productHelper.getAllProducts().then((places) => {
 
-    res.render('user/view-location', { places, banner, user: true });
+    res.render('user/view-location', { places, banner, user: true, footer:true, Account: true});
   })
 });
 router.get('/login', (req, res) => {
-  res.render('login')
+  res.render('login', { user: true})
 })
 router.get('/signup', (req, res) => {
-  res.render('signup')
+  res.render('signup', { user: true})
 })
 router.get('/back', (req, res) => {
   res.redirect('/')
 })
-
 module.exports = router;
